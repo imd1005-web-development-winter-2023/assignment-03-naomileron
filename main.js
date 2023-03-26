91d68592df9ded2b931fcea89a041f3d2d930bf2
Naomi Leron <leronnaomi@gmail.com>
2023-03-26 16:04:42 -0400

    not working

--- main.js
+++ main.js
@@ -11,6 +11,15 @@ window.addEventListener('load', () =>
     console.log("hit submit");
 
     const task_element = document.createElement("div");
-    task_element.classList.add("")
+    task_element.classList.add("task");
+
+    const task_content_element = document.createElement("div");
+    task_content_element.classList.add("content");
+    task_content_element.innerText = task; 
+
+    task_element.appendChild(task_content_element);
+
+    list_element.appendChild(task_element);
+
   })
 })
