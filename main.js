7483db8374937c8ea5089e81c9c863f2b785390d
Naomi Leron <leronnaomi@gmail.com>
2023-03-26 17:12:57 -0400

    working better but still not

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 window.addEventListener('load', () =>
 {
   const form = document.querySelector("#add-list-item");
-  const input = document.querySelector("#add-todo-item");
-  const list_el = document.querySelector("#tasks");
+  const input = document.querySelector("#new-todo-submission");
+  const list_element = document.querySelector("#tasks");
   console.log(form);
 
   form.addEventListener('submit', (event) => 
@@ -11,13 +11,14 @@ window.addEventListener('load', () =>
     console.log("hit submit");
 
     const task = input.value;
+    console.log(input);
 
     const task_element = document.createElement("div");
     task_element.classList.add("task");
 
     const task_content_element = document.createElement("div");
     task_content_element.classList.add("content");
-    task_content_element.innerText = "I am a task";
+    task_content_element.innerText = task;
     
 
     task_element.appendChild(task_content_element);
