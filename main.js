f7cfaefa5157f98b1d5148e8d98cdf18e5aa585d
Naomi Leron <leronnaomi@gmail.com>
2023-03-26 14:11:16 -0400

    start of js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+window.addEventListener('load', () =>
+{
+  const form = document.querySelector("#add-list-item");
+  const input = document.querySelector("#add-todo-item");
+  const list_el = document.querySelector("#tasks");
+  console.log(form);
 
+  form.addEventListener('submit', (e) => 
+  {
+
+  })
+})
